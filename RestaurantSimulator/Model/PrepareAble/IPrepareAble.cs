dd4deca1a9e8eafe1b73cb9e81ab17005f05a5ce
KLociJS <kisznerlorant21@gmail.com>
2023-04-15 20:22:05 +0200

    Add corresponding itnerfaces to last commit

---
+++ RestaurantSimulator/Model/PrepareAble/IPrepareAble.cs
@@ -0,0 +1,8 @@
+﻿namespace RestaurantSimulator.Model.PrepareAble;
+
+public interface IPrepareAble
+{
+    public string Name { get; init; }
+    public int Price { get; init; }
+    public List<Ingredient> Ingredients { get; init; }
+}
---
+++ RestaurantSimulator/Model/ServeAble/IServeAble.cs
@@ -0,0 +1,6 @@
+namespace RestaurantSimulator.Model.ServeAble;
+
+public interface IServeAble
+{
+    public string Name { get; init; }
+}
