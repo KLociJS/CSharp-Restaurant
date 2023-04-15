ed683270b191f2a219732a99f11a96967ffcc16e
KLociJS <kisznerlorant21@gmail.com>
2023-04-15 20:22:31 +0200

    Add restaurant class with TakeMenu method

---
+++ RestaurantSimulator/Service/Restaurant.cs
@@ -0,0 +1,38 @@
+﻿using RestaurantSimulator.Model.MenuProvider;
+
+namespace RestaurantSimulator.Service;
+
+public class Restaurant
+{
+    private readonly IMenuProvider _menuProvider;
+    
+    public Restaurant(IMenuProvider menuProvider)
+    {
+        _menuProvider = menuProvider;
+    }
+
+    public void TakeMenu()
+    {
+        var drinks = _menuProvider.Drinks;
+        var pizzas = _menuProvider.Pizzas;
+        var salads = _menuProvider.Salads;
+
+        Console.WriteLine("Drinks: ");
+        foreach (var drink in drinks)
+        {
+            Console.WriteLine(drink);
+        }
+
+        Console.WriteLine("Meals: ");
+        foreach (var pizza in pizzas)
+        {
+            Console.WriteLine(pizza);
+        }
+
+        Console.WriteLine("Salads: ");
+        foreach (var salad in salads)
+        {
+            Console.WriteLine(salad);
+        }
+    }
+}
