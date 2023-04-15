6e1be96367fe88880291389906c59ca92b2154d4
KLociJS <kisznerlorant21@gmail.com>
2023-04-15 17:28:02 +0200

    Add ingredient enums

---
+++ RestaurantSimulator/Model/IngredientEnums/IngredientEnum.cs
@@ -0,0 +1,24 @@
+﻿namespace RestaurantSimulator.Model.IngredientEnums;
+
+public enum IngredientEnum
+{
+    TomatoSauce,
+    MozzarellaCheese,
+    FreshBasil,
+    Pepperoni,
+    Ham,
+    Pineapple,
+    Sausage,
+    Bacon,
+    GroundBeef,
+    Mushrooms,
+    GreenPepper,
+    Onion,
+    Olive,
+    RomaineLettuce,
+    Crouton,
+    ParmesanCheese,
+    CaesarDressing,
+    SlicedTomatoes,
+    BalsamicGlaze,
+}
