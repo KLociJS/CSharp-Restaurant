a4c251c3bc6e7ec66db5d06ad031c57fcfc8c367
KLociJS <kisznerlorant21@gmail.com>
2023-04-15 18:09:17 +0200

    Add StockProvider interface

---
+++ RestaurantSimulator/Model/StockProvider/IStockProvider.cs
@@ -0,0 +1,8 @@
+﻿using RestaurantSimulator.Model.ServeAble;
+namespace RestaurantSimulator.Model.StockProvider;
+
+public interface IStockProvider
+{
+    public Dictionary<Ingredient,int> Ingredients { get; set; }
+    public Dictionary<Drink,int> Drinks { get; set; }
+}
